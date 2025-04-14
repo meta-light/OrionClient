6d169259556c4d5d1c5c480f1d8cc81d0e431e9e
meta-light <nick@carpinito.id>
2025-04-14 10:53:57 -0500

    Add Bitz Pool and Coin Modules

--- OrionClient_Lib/Pools/NoPool.cs
+++ OrionClient_Lib/Pools/NoPool.cs
@@ -1,53 +1,142 @@
-﻿//using OrionClientLib.Hashers.Models;
-//using OrionClientLib.Pools.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿// using OrionClientLib.Hashers.Models;
+// using OrionClientLib.Pools.Models;
+// using Solnet.Wallet;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-//namespace OrionClientLib.Pools
-//{
+// namespace OrionClientLib.Pools
+// {
 //    public class NoPool : IPool
 //    {
-//        public string PoolName { get; } = "No Pool";
+//        public string Name { get; } = "No Pool";
 //        public string DisplayName { get; } = "No Pool";
 //        public string Description { get; } = "Solo mining. [red]Not recommended[/] due to transaction fees";
-//        public bool HideOnPoolList { get; private set; } = false;
+//        public string ArgName { get; } = "nopool";
+//        public bool HideOnPoolList { get; } = false;
+//        public Coin Coins { get; } = Coin.Ore;
+//        public bool RequiresKeypair { get; } = true;
 
-//        public Dictionary<string, string> Features => throw new NotImplementedException();
+//        public Dictionary<string, string> Features => new Dictionary<string, string>
+//        {
+//            { "Solo Mining", "Mine directly to your wallet without a pool" },
+//            { "No Pool Fees", "Keep 100% of your mining rewards" },
+//            { "Direct Rewards", "Rewards go directly to your wallet" }
+//        };
 
 //        public event EventHandler<NewChallengeInfo> OnChallengeUpdate;
-//        public event EventHandler<string[]> OnMinerUpdate;
+//        public event EventHandler<(string[] columns, byte[] data)> OnMinerUpdate;
+//        public event EventHandler PauseMining;
+//        public event EventHandler ResumeMining;
 
-//        public Task<bool> ConnectAsync(string publicKey)
+//        private Wallet _wallet;
+//        private string _publicKey;
+//        private System.Timers.Timer _challengeTimer;
+//        private int _challengeId = 0;
+//        private byte[] _currentChallenge;
+//        private ulong _startNonce;
+//        private ulong _endNonce;
+//        private double _miningRewards = 0;
+//        private double _walletBalance = 0;
+
+//        public void SetWalletInfo(Wallet wallet, string publicKey)
 //        {
-//            throw new NotImplementedException();
+//            _wallet = wallet;
+//            _publicKey = publicKey;
 //        }
 
-//        public void DifficultyFound(DifficultyInfo info)
+//        public async Task<bool> ConnectAsync(CancellationToken token)
 //        {
-//            throw new NotImplementedException();
+//            // For solo mining, we don't need to connect to any pool
+//            // Just generate a new challenge periodically
+//            _challengeTimer = new System.Timers.Timer(30000); // 30 seconds
+//            _challengeTimer.Elapsed += (sender, e) => GenerateNewChallenge();
+//            _challengeTimer.Start();
+
+//            GenerateNewChallenge();
+//            return true;
 //        }
 
-//        public Task<bool> DisconnectAsync()
+//        public async Task<bool> DisconnectAsync()
 //        {
-//            throw new NotImplementedException();
+//            _challengeTimer?.Stop();
+//            _challengeTimer?.Dispose();
+//            return true;
 //        }
 
-//        public Task<double> GetFeeAsync()
+//        public async Task<double> GetFeeAsync(CancellationToken token)
 //        {
-//            throw new NotImplementedException();
+//            return 0.0; // No pool fees for solo mining
 //        }
 
-//        public Task<bool> SetupAsync()
+//        public async Task<(bool success, string errorMessage)> SetupAsync(CancellationToken token, bool initialSetup = false)
 //        {
-//            throw new NotImplementedException();
+//            if (_wallet == null)
+//            {
+//                return (false, "A full keypair is required for solo mining");
+//            }
+//            return (true, string.Empty);
+//        }
+
+//        public async Task<(bool success, string errorMessage)> OptionsAsync(CancellationToken token)
+//        {
+//            // For solo mining, there are no pool-specific options to configure
+//            return (true, string.Empty);
 //        }
 
 //        public string[] TableHeaders()
 //        {
-//            throw new NotImplementedException();
+//            return new[] { "Time", "Challenge ID", "Nonce Range", "Mining Rewards", "Wallet Balance" };
+//        }
+
+//        public void DifficultyFound(DifficultyInfo info)
+//        {
+//            // For solo mining, we don't need to track difficulty
+//            // The mining process will handle this
+//        }
+
+//        private void GenerateNewChallenge()
+//        {
+//            _challengeId++;
+//            _currentChallenge = new byte[32];
+//            new Random().NextBytes(_currentChallenge);
+//            _startNonce = 0;
+//            _endNonce = ulong.MaxValue;
+
+//            OnChallengeUpdate?.Invoke(this, new NewChallengeInfo
+//            {
+//                ChallengeId = _challengeId,
+//                Challenge = _currentChallenge,
+//                StartNonce = _startNonce,
+//                EndNonce = _endNonce,
+//                TotalCPUNonces = _endNonce / 2 // Split nonces evenly between CPU and GPU
+//            });
+
+//            // Update miner info with current rewards and balance
+//            OnMinerUpdate?.Invoke(this, (
+//                new[] { 
+//                    DateTime.Now.ToShortTimeString(),
+//                    _challengeId.ToString(),
+//                    $"{_startNonce}-{_endNonce}",
+//                    $"{_miningRewards:0.00000000000} {Coins}",
+//                    $"{_walletBalance:0.00000000000} {Coins}"
+//                },
+//                null
+//            ));
+//        }
+
+//        public void UpdateMiningRewards(double rewards)
+//        {
+//            _miningRewards = rewards;
+//            GenerateNewChallenge(); // Trigger UI update
+//        }
+
+//        public void UpdateWalletBalance(double balance)
+//        {
+//            _walletBalance = balance;
+//            GenerateNewChallenge(); // Trigger UI update
 //        }
 //    }
-//}
+// }
